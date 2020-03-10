e4b476dd4ba0ce94cd6b300d83bcb688da5f100d
Jimmy Pham <jpham35@gatech.edu>
2020-03-10 10:38:44 -0400

    forgot semicolon

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var members = ["RM", "Jin", "Suga", "J-Hope", "Jimin", "V", "Jungkook"]
+var members = ["RM", "Jin", "Suga", "J-Hope", "Jimin", "V", "Jungkook"];
 
 d3.csv("dataset.csv").then((data) => {
     console.log(data[0]);
