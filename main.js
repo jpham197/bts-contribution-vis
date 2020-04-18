864bdb8346f075f3eca45ad631ba660aab76875e
Hang Qiu <924920329@qq.com>
2020-04-18 19:52:47 -0400

    fix issue #58 and #64

--- main.js
+++ main.js
@@ -956,3 +956,25 @@ function filter(value) {
         }
     renderBars(filter_data, member);
 }
+function replaceButtonText(buttonId, text)
+{
+  if (document.getElementById)
+  {
+    var button=document.getElementById(buttonId);
+    if (button)
+    {
+      if (button.childNodes[0])
+      {
+        button.childNodes[0].nodeValue=text;
+      }
+      else if (button.value)
+      {
+        button.value=text;
+      }
+      else //if (button.innerHTML)
+      {
+        button.innerHTML=text;
+      }
+    }
+  }
+}
